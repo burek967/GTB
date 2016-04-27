df079c5d7f15a481244714a83e081660b887a9d5
Angelika Serwa <graszka223@gmail.com>
2016-04-27 22:36:36 +0200

    vertex dragging

--- src/gtb/controller/mouse/MoveMode.java
+++ src/gtb/controller/mouse/MoveMode.java
@@ -1,6 +1,8 @@
 package gtb.controller.mouse;
 
 import gtb.model.Graph;
+import gtb.model.Position;
+import gtb.model.Vertex;
 import gtb.view.GraphRenderer;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.input.ScrollEvent;
@@ -9,24 +11,34 @@ public class MoveMode implements MouseMode {
 
     private int lastX = -1;
     private int lastY = -1;
+    private Vertex vertexDragged = null;
 
     @Override
     public void onPress(MouseEvent event, GraphRenderer renderer, Graph graph) {
         lastX = (int) event.getX();
         lastY = (int) event.getY();
+        vertexDragged = renderer.getVertexAt(new Position(lastX, lastY));
     }
 
     @Override
     public void onRelease(MouseEvent event, GraphRenderer renderer, Graph graph) {
         lastX = lastY = -1;
+        vertexDragged = null;
     }
 
     @Override
     public void onDrag(MouseEvent event, GraphRenderer renderer, Graph graph) {
-        if(lastX != -1)
-            renderer.changeOffset(lastX - (int) event.getX(), lastY - (int) event.getY());
-        lastX = (int) event.getX();
-        lastY = (int) event.getY();
+        if(vertexDragged != null) {
+            vertexDragged.getData().setPosition(renderer.getPositionAt(
+                    new Position((float)event.getX(), (float)event.getY())));
+            renderer.redraw();
+        }
+        else {
+            if (lastX != -1)
+                renderer.changeOffset(lastX - (int) event.getX(), lastY - (int) event.getY());
+            lastX = (int) event.getX();
+            lastY = (int) event.getY();
+        }
     }
 
     public void onScroll(ScrollEvent event, GraphRenderer renderer){
