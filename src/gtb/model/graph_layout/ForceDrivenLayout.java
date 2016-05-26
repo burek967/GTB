ce2c65884c0f8306e88939d7b910eaea163eb305
Angelika Serwa <graszka223@gmail.com>
2016-05-26 02:46:36 +0200

    now also works on directed edges

--- src/gtb/model/graph_layout/ForceDrivenLayout.java
+++ src/gtb/model/graph_layout/ForceDrivenLayout.java
@@ -1,8 +1,12 @@
 package gtb.model.graph_layout;
 
+import gtb.model.Edge;
 import gtb.model.Graph;
 import gtb.model.Position;
 import gtb.model.Vertex;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Algorytm o gigantycznej złożoności (na razie)
@@ -10,14 +14,15 @@ import gtb.model.Vertex;
  * Created by angela on 5/19/16.
  */
 public class ForceDrivenLayout implements GraphLayout {
+    private HashMap<Vertex, ArrayList<Vertex>> hm;
+
     public void tick(Graph g) {
         for (Vertex v1 : g.getVertices()) {
             Position p1 = v1.getData().getPosition();
             //suma sił działających na wierzchołek
             float xf = 0, yf = 0;
             //połączone zachowują się jak sprężyna - prawo hooka
-            for (Vertex v2 : g.getVerticesConnectedWith(v1)) {
-
+            for (Vertex v2 : hm.get(v1)) {
                 if (v1 == v2) continue;
                 Position p2 = v2.getData().getPosition();
                 double dx = p2.getX() - p1.getX();
@@ -44,6 +49,14 @@ public class ForceDrivenLayout implements GraphLayout {
 
     @Override
     public void layoutGraph(Graph g) {
+        hm = new HashMap<>();
+        for(Vertex v : g.getVertices()) {
+            hm.put(v, new ArrayList<>());
+        }
+        for(Edge e: g.getEdges()) {
+            hm.get(e.getFirstVertex()).add(e.getSecondVertex());
+            hm.get(e.getSecondVertex()).add(e.getFirstVertex());
+        }
         tick(g);
     }
 }
